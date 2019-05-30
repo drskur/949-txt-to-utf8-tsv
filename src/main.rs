f2963ab09b773e9221405f5bb3421f53ad410963
drskur <drskur@me.com>
2019-05-30 13:00:44 +0900

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+#[macro_use] extern crate clap;
+
 use std::fs::{File};
 use std::io::{Result, BufReader, BufRead, BufWriter, Write};
 use encoding::all::WINDOWS_949;
 use encoding::types::Encoding;
 use encoding::DecoderTrap;
+use clap::App;
 
 fn main() -> Result<()> {
-    let path = "mart_djy_03.txt";
+    let yaml = load_yaml!("../cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let path = matches.value_of("FILE").unwrap();
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
 
-    let tsv = File::create("mart_djy_03.tsv")?;
+    let output_path = matches.value_of("OUTPUT").unwrap();
+    let tsv = File::create(output_path)?;
     let mut writer = BufWriter::new(tsv);
 
     loop {
